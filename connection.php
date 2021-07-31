2fb8eaf2759c2aa62e73580214102bd8746badf1
ryowuandjanet <ryowu0329@gmail.com>
2021-08-01 00:46:21 +0800

    add connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
+//獲取Heroku ClearDB連接信息
+$cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+$cleardb_server = $cleardb_url [ "host" ] ;  
+$cleardb_username = $cleardb_url [ "user" ] ;  
+$cleardb_password = $cleardb_url [ "pass" ] ;  
+$cleardb_db = substr ( $cleardb_url [ "path" ] , 1 ) ;  
+$active_group = '默認' ;  
+$query_builder = TRUE ;  
+// 連接數據庫
+$conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ;     
+?>
 
-$connect = mysqli_connect("localhost","root","","ecomm");
 
