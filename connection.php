af52e1672ae5aa2163c3405901e5352f3cb1d37a
ryowuandjanet <ryowu0329@gmail.com>
2021-08-01 00:49:11 +0800

    set database

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 //獲取Heroku ClearDB連接信息
 $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
-$cleardb_server = $cleardb_url [ "host" ] ;  
-$cleardb_username = $cleardb_url [ "user" ] ;  
-$cleardb_password = $cleardb_url [ "pass" ] ;  
+$cleardb_server = $cleardb_url [ "us-cdbr-east-04.cleardb.com" ] ;  
+$cleardb_username = $cleardb_url [ "b76b9adcbf0e1e" ] ;  
+$cleardb_password = $cleardb_url [ "f3cf4696" ] ;  
 $cleardb_db = substr ( $cleardb_url [ "path" ] , 1 ) ;  
 $active_group = '默認' ;  
 $query_builder = TRUE ;  
