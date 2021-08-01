5479cd4955c5c6befc2f02199f113fdd971a9c73
ryowuandjanet <ryowu0329@gmail.com>
2021-08-01 10:30:08 +0800

    add dbname

--- connection.php
+++ connection.php
@@ -1,14 +1,13 @@
 <?php
-//獲取Heroku ClearDB連接信息
-$cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
-$cleardb_server = $cleardb_url [ "us-cdbr-east-04.cleardb.com" ] ;  
-$cleardb_username = $cleardb_url [ "b76b9adcbf0e1e" ] ;  
-$cleardb_password = $cleardb_url [ "f3cf4696" ] ;  
-$cleardb_db = substr ( $cleardb_url [ "path" ] , 1 ) ;  
-$active_group = '默認' ;  
-$query_builder = TRUE ;  
-// 連接數據庫
-$conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ;     
+  //獲取Heroku ClearDB連接信息
+  $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+  $cleardb_server = $cleardb_url [ "us-cdbr-east-04.cleardb.com" ] ;  
+  $cleardb_username = $cleardb_url [ "b76b9adcbf0e1e" ] ;  
+  $cleardb_password = $cleardb_url [ "f3cf4696" ] ;  
+  $cleardb_db = substr ( $cleardb_url [ "heroku_00389e1c064314c" ] , 1 ) ;  
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to DB
+  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
-
 
